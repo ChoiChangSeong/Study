d560355eebd19ac15dde3bc6257d7eec1aee5196
Dean <xvz1126@gmail.com>
2022-03-12 14:35:49 +0900

    All Table Create and insert complete

--- src/main/java/com/study_java/main/TestApplication.java
+++ src/main/java/com/study_java/main/TestApplication.java
@@ -1,10 +1,11 @@
-package com.study_java.test;
+package com.study_java.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.study_java")
 public class TestApplication {
     public static void main(String[] args){
         SpringApplication.run(TestApplication.class, args);
---
+++ src/test/java/com/study_java/main/TestApplication.java
@@ -0,0 +1,12 @@
+package com.study_java.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
